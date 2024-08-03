864035c050800053acf981f0ec8924018765a3a1
Sohini <purkaitsohini20@gmail.com>
2024-08-03 11:06:22 +0530

    math

---
+++ math.java
@@ -0,0 +1,10 @@
+class math{
+    public static void main(String args[])
+    {
+        System.out.println(Math.max(5, 6)); //6
+        System.out.println(Math.min(5, 6)); //5
+        //renadom returns number between range 0.0 to 1.0
+        System.out.println(Math.random());
+        System.out.println((int)(Math.random()*100));
+    }    
+}
