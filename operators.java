f2e9f323c027157befbfa8376b8d554c2df3018e
Sohini <purkaitsohini20@gmail.com>
2024-08-03 10:57:30 +0530

    operators

---
+++ operators.java
@@ -0,0 +1,18 @@
+class operators{
+    public static void main(String args[])
+    {
+       int a = 1;
+       int b = 2;
+       int div = a / b;
+       System.out.println(div); // 0 not 0.5 since int
+
+       //unary operator
+       int num = 1;
+       System.out.println(num++); //1
+       System.out.println(num); //2
+       System.out.println(++num); //3
+       
+
+       
+    }    
+}
