d91c866c67d95289720a3f0b181bace5ee48bae0
Sohini <purkaitsohini20@gmail.com>
2024-08-03 13:56:19 +0530

    break and continue

--- breakAndCont.java
+++ breakAndCont.java
@@ -2,13 +2,13 @@ class breakAndCont{
     public static void main(String args[])
     {
         int i = 0;
-        while(true){
+        while(true){         
+            System.out.println(i);
+            i++;
             if(i == 2){
                 i++;
                 continue;
             }
-            System.out.println(i);
-            i++;
             if(i > 5){
                 break;
             }
