a70fc24c0678001e713aba07db02b26fa2b556b9
Sohini <purkaitsohini20@gmail.com>
2024-08-03 09:57:58 +0530

    constants

---
+++ constants.java
@@ -0,0 +1,9 @@
+class constants{
+    public static void main(String args[])
+    {
+        final float PI = 3.14F;
+
+        //use final keyword and conentionally variable name is uppercase for constants
+        
+    }    
+}
